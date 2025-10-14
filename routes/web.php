af20c77f44f88ce9689f1e27c053f60d9eaddc9b
UmarUPN <23082010087@student.upnjatim.ac.id>
2025-10-14 21:40:08 +0700

    Tugas.2.Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\GenreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/books', [BookController::class, 'index']);
+route::get('/authors', [AuthorController::class, 'index']);
+route::get('/genres', [GenreController::class, 'index']);
