ef95959fa74c56c898629bd3d4266cab65b1adfa
UmarUPN <23082010087@student.upnjatim.ac.id>
2025-10-24 22:01:31 +0700

    temp.1

--- routes/api.php
+++ routes/api.php
@@ -17,21 +17,29 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 // Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
+// ==========================================================
+// Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
+// Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
+// Route::apiResource('/books', BookController::class)->only(['index', 'show']);
 
-Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
-Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
-Route::apiResource('/books', BookController::class)->only(['index', 'show']);
+
+// Route::middleware(['auth:api'])->group(function () {
+//     Route::apiResource('/transactions', TransactionController::class)->only(['store', 'update', 'show']);
+
+//     Route::middleware(['role:admin'])->group(function () {
+//         Route::apiResource('/authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+//         Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
+//         Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
+//         Route::apiResource('/transactions', TransactionController::class)->only(['index', 'destroy']);
+//     });
+
+//     Route::post('/logout', [AuthController::class, 'logout']);
+// });
+// ==========================================================
 
 
-Route::middleware(['auth:api'])->group(function () {
-    Route::apiResource('/transactions', TransactionController::class)->only(['store', 'update', 'show']);
 
-    Route::middleware(['role:admin'])->group(function () {
-        Route::apiResource('/authors', AuthorController::class)->only(['store', 'update', 'destroy']);
-        Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
-        Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
-        Route::apiResource('/transactions', TransactionController::class)->only(['index', 'destroy']);
-    });
-
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+Route::apiResource('/authors', AuthorController::class);
+Route::apiResource('/genres', GenreController::class);
+Route::apiResource('/books', BookController::class);
+Route::apiResource('/transactions', TransactionController::class);
